74020b4f2c22200cd66330b74036f3de7776185a
Gitesh Kumar <gitesh.kumar@BRILLIO.COM>
2019-11-05 16:49:32 +0530

    Creating pkg branch and shellscript for Package install

---
+++ build/UATpackage-Install.sh
@@ -0,0 +1,16 @@
+echo "Unlocked Package process started..."
+Package_Version=""
+TARGET_ORG=
+
+echo "Creating new package version for BrillioTest"
+  Package_Version="$(sfdx force:package:version:create -p PACKAGE1 -d force-app -k test1234 -w 10 -v DEV --json | jq '.result.SubscriberPackageVersionId' | tr -d '"')"
+  sleep 300 # We've to wait for package replication.
+echo "Package version is Done!"
+echo "******************************"
+echo  $Package_Version
+echo "******************************"
+
+echo "Package installation process started"
+sfdx force:package:install --package $Package_Version -w 10 -r -k test1234 -u UAT
+
+echo "Installed Sucessfully...Done!"
